6f7ac2480e9e722737bf388a6d9681040c4ce49e
jtreeves <jackson.t.reeves@gmail.com>
2022-12-21 13:32:12 -0500

    [css] Center all content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Header from './components/Header'
 import Calendar from './components/Calendar'
 import Description from './components/Description'
+import './styles/main.css'
 
 function App() {
     return (
-        <>
+        <main>
             <Header />
             <Calendar />
             <Description />
-        </>
+        </main>
     )
 }
 
